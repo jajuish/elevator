8c4de0861a0d344f6bf9001aa2b47005d7bd0a72
Ishita Jaju <ishijaju@gmail.com>
2022-05-05 15:03:41 +0200

    Test calling function

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
-/*
-Requirements
-Number of floors: 200
-Number of elevators in a building: 2
-Max passenger-carrying capacity: 400kg
-Need to be secure and operational.
-*/
-
 // TODO: handle that floor is already pressed
 
-import BuildingSystem from "./src/system_deprecated";
+import BuildingSystem from "./src/system";
 
-const system = new BuildingSystem(2, 200, 400)
+const elevator = new BuildingSystem()
 
-// setTimeout(() => { system.call(2, 1) }, 6000)
-// setTimeout(() => { system.call(1, -1) }, 8000)
-// setTimeout(() => { system.call(3, 1) }, 10000)
-
-system.call(2, 1)
-system.call(1, -1)
-system.call(3, 1)
+setTimeout(() => { elevator.call(2, 1) }, 1000)
+setTimeout(() => { elevator.call(1, -1) }, 2000)
+setTimeout(() => { elevator.boardPeople(2, 1, [4]) }, 2000)
+setTimeout(() => { elevator.call(4, 1) }, 3000)
+setTimeout(() => { elevator.call(8, 1) }, 4000)
+setTimeout(() => { elevator.call(16, -1) }, 5000)
+setTimeout(() => { elevator.call(31, 1) }, 6000)
+setTimeout(() => { elevator.call(36, 1) }, 7000)
+setTimeout(() => { elevator.call(43, -1) }, 8000)
+setTimeout(() => { elevator.call(38, 1) }, 9000)
+setTimeout(() => { elevator.call(131, -1) }, 10000)
+setTimeout(() => { elevator.call(68, 1) }, 11000)
+setTimeout(() => { elevator.call(90, -1) }, 12000)
+setTimeout(() => { elevator.call(92, 1) }, 13000)
+setTimeout(() => { elevator.call(188, -1) }, 14000)
+setTimeout(() => { elevator.call(0, 1) }, 14000)
+setTimeout(() => { elevator.call(35, -1) }, 16000)
+setTimeout(() => { elevator.call(49, -1) }, 17000)
